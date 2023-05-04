a986056cb8395c488820fbdcc64a3f80b4159bf6
Kshitij Rangari <kshitij.rangari@viacomcontractor.com>
2023-05-04 15:37:56 -0400

    setup basic layout and of portfolio

---
+++ src/components/about/About.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>5+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Hi, I am Kshitij Rangari.
+            <br></br>
+            Software Developer, based in Toronto
+            I specialize in web technology and building real world web applications with fantastic user experience and providing highly scalable software solutions.
+            Born and raised in Mumbai, India..I am a technology enthusiast and carry passion for innovation and new ideas. I am an avid traveler and a ardent Photographer.
+
+            "It's easier to be Committed when you are Passionate"
+
+            Let's connect and share some wonderful ideas to make world a better place :)
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
